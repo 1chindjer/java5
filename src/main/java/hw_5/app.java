bf8aa8607c958f0c8fe9f523dad27b489a2a8df6
1chindjer <chindjer@gmail.com>
2023-07-12 21:52:56 +0300

    Аттестация

--- src/main/java/hw_5/app.java
+++ src/main/java/hw_5/app.java
@@ -1,38 +1,65 @@
 package hw_5;
 import java.util.*;
 
+class Cat {
+    private String name;
+    private int age;
 
-public class app {
-    private final Map<String, List<String>> directory;
-
-    public app() {
-        this.directory = new HashMap<>();
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    public void add(String lastName, String phoneNumber) {
-        if (directory.containsKey(lastName)) {
-            List<String> phones = directory.get(lastName);
-            if (!phones.contains(phoneNumber)) {
-                phones.add(phoneNumber);
-            }
-        } else {
-            List<String> phones = new ArrayList<>();
-            phones.add(phoneNumber);
-            directory.put(lastName, phones);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
         }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cat cat = (Cat) o;
+        return age == cat.age && Objects.equals(name, cat.name);
     }
 
-    public List<String> get(String lastName) {
-        return directory.get(lastName);
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 
-    public static void main(String[] args) {
-        app app = new app();
-        app.add("Петров", "123335456789");
-        app.add("Иванов", "98763254321");
-        app.add("Петров", "121224");
-
-        System.out.println("Петров номер: " + app.get("Петров"));
-        System.out.println("Иванов номер: " + app.get("Иванов"));
+    @Override
+    public String toString() {
+        return "Кот " +
+                "имя='" + name + '\'' +
+                ", возраст=" + age +
+                ' ';
     }
 }
+
+public class app {
+
+        public static void main(String[] args) {
+            Cat cat1 = new Cat("Рыжик", 3);
+            Cat cat2 = new Cat("Пыжик", 2);
+            Cat cat3 = new Cat("Рыжик", 3); 
+            Cat cat4 = new Cat("Васька", 2);
+            Cat cat5 = new Cat("Пыжик", 2);
+    
+            Cat[] catsArray = new Cat[]{cat1, cat2, cat3, cat4, cat5};
+    
+            Set<Cat> uniqueCatsSet = new LinkedHashSet<>();
+            Set<Cat> duplicateCatsSet = new LinkedHashSet<>();
+    
+            for (Cat cat : catsArray) {
+                if (!uniqueCatsSet.add(cat)) {
+                    duplicateCatsSet.add(cat);
+                }
+            }
+    
+            System.out.println("Количество дубликатов: " + duplicateCatsSet.size());
+            System.out.println("Дубликаты: " + duplicateCatsSet);
+    
+            System.out.println("Уникальные коты: " + uniqueCatsSet);
+        }
+    }
+    
